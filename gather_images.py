91884880e8ec65f636efaa910870a642b174f8b4
rahulomprasad <61745278+rahulomprasad@users.noreply.github.com>
2021-01-21 16:46:07 +0530

    Update gather_images.py

--- gather_images.py
+++ gather_images.py
@@ -1,12 +1,11 @@
 
 
+
 import cv2
 import os
-import sys
 
-
-label_name="rock..."
-IMG_SAVE_PATH = 'image_data'
+label_name="scissors"
+IMG_SAVE_PATH = 'image'
 IMG_CLASS_PATH = os.path.join(IMG_SAVE_PATH, label_name)
 
 cap = cv2.VideoCapture(0)
@@ -19,17 +18,18 @@ while True:
     if not ret:
         continue
 
-    if count == 200:
+    if count == 2000:
         break
 
-    cv2.rectangle(frame, (100, 100), (400, 400), (255, 255, 255), 2)
+    cv2.rectangle(frame, (100, 100), (800, 500), (255, 255, 255), 2)
+    roi = frame[100:500, 100:800]
 
     if start:
-        roi = frame[100:500, 100:500]
+        
         save_path = os.path.join(IMG_CLASS_PATH, '{}.jpg'.format(count + 1))
         cv2.imwrite(save_path, roi)
         count += 1
-        start= not start
+        start=not start
 
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, "Collecting {}".format(count),
@@ -43,6 +43,8 @@ while True:
     if k %256==27:
         break
 
+
 print("\n{} image(s) saved to {}".format(count, IMG_CLASS_PATH))
+
 cap.release()
 cv2.destroyAllWindows()
