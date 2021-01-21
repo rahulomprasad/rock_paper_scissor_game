cbdb38d5d0ca88bd77f23e8aad1d3509d1e0f561
rahulomprasad <61745278+rahulomprasad@users.noreply.github.com>
2021-01-21 16:44:20 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,19 +1,23 @@
 import cv2
+
 import numpy as np
-from keras_squeezenet import SqueezeNet
+
 from keras.optimizers import Adam
-from keras.utils import np_utils
-from keras.layers import Activation, Dropout, Convolution2D, GlobalAveragePooling2D, Dense
+
+from keras.utils import to_categorical
+
 from keras.models import Sequential
-import tensorflow as tf
+
+from keras.layers import Dense,Conv2D,Flatten,MaxPooling2D
+
 import os
 
-IMG_SAVE_PATH = 'image_data'
+IMG_SAVE_PATH = 'image'
 
 CLASS_MAP = {
     "rock": 0,
     "paper": 1,
-    "scissor": 2,
+    "scissors": 2,
     "none": 3
 }
 
@@ -25,15 +29,25 @@ def mapper(val):
 
 
 def get_model():
-    model = Sequential([
-        SqueezeNet(input_shape=(227, 227, 3), include_top=False),
-        Dropout(0.5),
-        Convolution2D(4, (1, 1), padding='valid'),
-        Activation('relu'),
-        GlobalAveragePooling2D(),
-        Dense(4,activation='softmax')
-    ])
-    return model
+    
+    CNN_model=Sequential()
+    #Performing Convolution
+    CNN_model.add(Conv2D(64,kernel_size=3,activation='relu',input_shape=(28,28,1)))
+    #Performing pooling
+    CNN_model.add(MaxPooling2D(pool_size=(2,2)))
+    #Performing Convolution
+    CNN_model.add(Conv2D(64,kernel_size=3,activation='relu'))
+    #Performin pooling 
+    CNN_model.add(MaxPooling2D(pool_size=(2,2)))
+    #Performing Convolution
+    CNN_model.add(Conv2D(64,kernel_size=3,activation='relu',input_shape=(28,28,1)))
+    #Performing pooling
+    CNN_model.add(MaxPooling2D(pool_size=(2,2)))
+    #Performing Flattening
+    CNN_model.add(Flatten())
+    #Adding numbers of nodes in hidden 
+    CNN_model.add(Dense(4,activation='softmax'))
+    return CNN_model
 
 
 # load images from the directory
@@ -48,27 +62,19 @@ for directory in os.listdir(IMG_SAVE_PATH):
             continue
         img = cv2.imread(os.path.join(path, item))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (227, 227))
+        img = cv2.resize(img, (28, 28))
+        img = img[:,:,0]
         dataset.append([img, directory])
 
-'''
-dataset = [
-    [[...], 'rock'],
-    [[...], 'paper'],
-    ...
-]
-'''
+
+#preprocessing data
 data, labels = zip(*dataset)
 labels = list(map(mapper, labels))
+data = np.asarray(data)
+#reshape to specific format
+data=data.reshape(8000,28,28,1)
+labels= to_categorical(labels)
 
-
-'''
-labels: rock,paper,paper,scissors,rock...
-one hot encoded: [1,0,0], [0,1,0], [0,1,0], [0,0,1], [1,0,0]...
-'''
-
-# one hot encode the labels
-labels = np_utils.to_categorical(labels)
 
 # define the model
 model = get_model()
@@ -79,7 +85,7 @@ model.compile(
 )
 
 # start training
-model.fit(np.array(data), np.array(labels), epochs=10)
+model.fit(np.array(data), np.array(labels), epochs=25)
 
 # save the model for later use
 model.save("rock-paper-scissors-model.h5")
